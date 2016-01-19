9a8248d77efd2f56be51c2dbd49a298cab3ab334
thegege <the_gege@hotmail.com>
2016-01-19 21:54:48 +0100

    pff

--- hotfixes/broadcast_text_locale.sql
+++
@@ -1,9 +0,0 @@
-DELETE FROM `broadcast_text_locale` WHERE `locale` = 'koKR';
-DELETE FROM `broadcast_text_locale` WHERE `locale` = 'deDE';
-DELETE FROM `broadcast_text_locale` WHERE `locale` = 'zhCN';
-DELETE FROM `broadcast_text_locale` WHERE `locale` = 'zhTW';
-DELETE FROM `broadcast_text_locale` WHERE `locale` = 'esES';
-DELETE FROM `broadcast_text_locale` WHERE `locale` = 'esMX';
-DELETE FROM `broadcast_text_locale` WHERE `locale` = 'itIT';
-DELETE FROM `broadcast_text_locale` WHERE `locale` = 'ruRU';
-DELETE FROM `broadcast_text_locale` WHERE `locale` = 'ptBR';
